34e7e45d9fc3ecb0dba7bcdc87916e90bb785e86
Egor <egor_usachev@mail.ru>
2026-02-10 11:36:29 +0300

    Убрал каталог с хедера

--- local/templates/dresscodeV2/components/bitrix/menu/topMenu6/template.php
+++ local/templates/dresscodeV2/components/bitrix/menu/topMenu6/template.php
@@ -5,6 +5,11 @@
 		<?foreach($arResult as $arItem):
 			if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1)
 				continue;
+
+			// Пропускаем элемент "Каталог"
+			if ($arItem["TEXT"] === "Каталог"):
+				continue;
+			endif;
 		?>
 			<?if($arItem["SELECTED"]):?>
 				<li><a href="<?=$arItem["LINK"]?>" class="selected"><?=$arItem["TEXT"]?></a></li>
